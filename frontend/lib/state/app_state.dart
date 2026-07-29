8c728321ee39282734d9090f0611c109f1aaeb64
Rithish R <rithish0637@gmail.com>
2026-07-29 12:46:19 +0530

    Firebase Implemented

--- frontend/lib/state/app_state.dart
+++ frontend/lib/state/app_state.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import '../data/mock_data.dart';
 import '../models/models.dart';
 
